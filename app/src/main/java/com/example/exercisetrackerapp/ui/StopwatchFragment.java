9134a17a250eb047857c83fab5bdf6ea375daaf2
Cristina Cazares <ccazares8@uabc.edu.mx>
2019-11-10 19:43:14 -0800

    US 3: Task: Include a stopwatch to mesaure the time of the activity

--- app/src/main/java/com/example/exercisetrackerapp/ui/StopwatchFragment.java
+++ app/src/main/java/com/example/exercisetrackerapp/ui/StopwatchFragment.java
@@ -117,4 +117,6 @@ public class StopwatchFragment extends Fragment implements View.OnClickListener{
         }
 
     }
+
+
 }
--- app/src/main/java/com/example/exercisetrackerapp/ui/TrackExerciseActivity.java
+++ app/src/main/java/com/example/exercisetrackerapp/ui/TrackExerciseActivity.java
@@ -1,13 +1,11 @@
 package com.example.exercisetrackerapp.ui;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import com.example.exercisetrackerapp.R;
 
-public class TrackExerciseActivity extends AppCompatActivity {
+public class TrackExerciseActivity extends AppCompatActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -15,10 +13,7 @@ public class TrackExerciseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_exercise);
 
-
     }
-
-
 
 
 }
