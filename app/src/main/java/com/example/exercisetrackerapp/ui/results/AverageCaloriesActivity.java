6b22de9e478e2e678f60a7cec7dbc88ba3be8ff7
Pamela-Itzelt <a1245064@uabc.edu.mx>
2019-12-03 00:16:57 -0800

    Avance U12. Promedio de calorías gastadas
    - Calorías por metabolismo basal según periodo de tiempo
    - Creación de clase para evaluar rangos de fecha

--- app/src/main/java/com/example/exercisetrackerapp/ui/results/AverageCaloriesActivity.java
+++ app/src/main/java/com/example/exercisetrackerapp/ui/results/AverageCaloriesActivity.java
@@ -3,10 +3,13 @@ package com.example.exercisetrackerapp.ui.results;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.exercisetrackerapp.R;
+import com.example.exercisetrackerapp.ui.burnedCalories.BurnedCalories;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -24,21 +27,26 @@ import java.util.Calendar;
 
 public class AverageCaloriesActivity  extends AppCompatActivity {
 
+    //VARIABLES
     private Spinner spinner;
+    private int range;
+    float peso, altura, total = 0, calorias, i = 1, edad;
+    Calendar cale = Calendar.getInstance();
+    int year = cale.get(Calendar.YEAR);
+    String genero;
+    TextView basal, cal;
+    ArrayList<BurnedCalories> burnedCalories = new ArrayList<BurnedCalories>() ;
 
+    //GRAFICOS
     private LineChart lineChart;
     private LineDataSet lineDataSet;
 
+    //BASE DE DATOS
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    //referencia al usuario
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     String email, emailAux;
-    float peso, altura, total=0,calorias,i=1,edad;
-    Calendar cale= Calendar.getInstance();
-    int year= cale.get(Calendar.YEAR);
-    String genero;
-    TextView basal,cal;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +54,74 @@ public class AverageCaloriesActivity  extends AppCompatActivity {
 
         if (user != null) {
             email = user.getEmail();
-
         }
-        basal = (TextView) findViewById(R.id.textView7);
-        cal = (TextView) findViewById(R.id.textView14);
+        basal = (TextView) findViewById(R.id.numMBasal);
+        cal = (TextView) findViewById(R.id.numKcal);
+
         inicializarFirebase();
+        getCaloriesBurned();
+
+        //Obtener spinner seleccionado
+        spinner = (Spinner) findViewById(R.id.spinnerExercise);
+        //Listener. Realizar cambios si se cambia el valor del spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                range = getRange(spinner.getSelectedItem().toString());
+                setGraphic();
+                setBasalCalories();
+                //Toast.makeText(parentView.getContext(), String.valueOf(range) , Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {}
+        });
+    }
+
+    private int getRange(String period) {
+        switch (period) {
+            case "Hoy":
+                return 24;
+            case "Semana":
+                return 7;
+            case "Mes":
+                return 30;
+            case "Año":
+                return 365;
+        }
+        return 0;
+    }
+
+    void setGraphic() {
+        // Enlazamos al XML
+        lineChart = this.findViewById(R.id.lineChart);
+
+        // Recolectamos el set de datos
+        ArrayList<Entry> lineEntries = new ArrayList<Entry>();
+
+
+        // -----  MOMENTANEO  --------------------------
+
+        lineEntries.add(new Entry((float) 0, (float) 2));
+        lineEntries.add(new Entry((float) 1, (float) 0));
+
+        // -----  MOMENTANEO  --------------------------
+
+
+        // disable description text
+        lineChart.getDescription().setEnabled(false);
+        // Unimos los datos al data set
+        lineDataSet = new LineDataSet(lineEntries, "Calorías gastadas");
+        // Asociamos al gráfico
+        LineData lineData = new LineData();
+        lineData.addDataSet(lineDataSet);
+        lineChart.setData(lineData);
+
+        // draw points over time
+        lineChart.animateX(1500);
+    }
+
+    void setBasalCalories(){
         databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -62,23 +133,30 @@ public class AverageCaloriesActivity  extends AppCompatActivity {
                         genero = areaSnapshot.child("genero").getValue().toString();
                         edad = Float.parseFloat(areaSnapshot.child("fecha").child("year").getValue().toString());
                         edad = edad - year;
-                        if (genero.equals("Masculino")){
-                            total = (float) ((10*peso) + (6.25*altura*100)- (5*edad ) + 5);
-                        }
-                        else{
-                            total= (float) ((10*peso)  + (6.25*altura*100) - (5*edad) -161);
 
+                        if (genero.equals("Masculino")) {
+                            total = (float) ((10 * peso) + (6.25 * altura * 100) - (5 * edad) + 5);
+                        } else {
+                            total = (float) ((10 * peso) + (6.25 * altura * 100) - (5 * edad) - 161);
                         }
-                        basal.setText(Float.toString(total));
+                        if(range==24)
+                            basal.setText(Float.toString(total)); //cantidad de un solo dia
+                        else
+                            basal.setText(Float.toString(total*range)); //por cantidad de dias
                     }
                 }
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+    }
+
+    //Funcion que almacena todas las calorias quemadas de los ultimos dos años
+    private void getCaloriesBurned(){
+
+        //BurnedCalories(String usuario, float cantCalorias, Date date,String ejercicio)
 
         databaseReference.child("caloriasQuemadas").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -86,45 +164,24 @@ public class AverageCaloriesActivity  extends AppCompatActivity {
                 for (DataSnapshot areaSnapshot : dataSnapshot.getChildren()) {
                     emailAux = areaSnapshot.child("usuario").getValue().toString();
                     if (emailAux.equals(email)) {
-
                         calorias = calorias + Float.parseFloat(areaSnapshot.child("cantCalorie").getValue().toString());
-                        calorias = calorias/i;
+                        calorias = calorias / i;
                         cal.setText(Float.toString(calorias));
                         i++;
                     }
                 }
-
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
         });
-
-
-        spinner = (Spinner) findViewById(R.id.spinnerExercise);
-
-        // Enlazamos al XML
-        lineChart = this.findViewById(R.id.lineChart);
-
-        // Recolectamos el set de datos
-        ArrayList<Entry> lineEntries = new ArrayList<Entry>();
-
-        // -----  MOMENTANEO  --------------------------
-        lineEntries.add(new Entry((float) 0,(float)2));
-        lineEntries.add(new Entry((float) 1,(float)0));
-
-        // Unimos los datos al data set
-        lineDataSet = new LineDataSet(lineEntries, "Platzi");
-
-        // Asociamos al gráfico
-        LineData lineData = new LineData();
-        lineData.addDataSet(lineDataSet);
-        lineChart.setData(lineData);
     }
-    private void inicializarFirebase () {
+
+    private void inicializarFirebase() {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
     }
 
 }
+
+
---
+++ app/src/test/java/com/example/exercisetrackerapp/DateValidatorRangeTest.java
@@ -0,0 +1,39 @@
+package com.example.exercisetrackerapp;
+
+import com.example.exercisetrackerapp.ui.results.DateValidator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class DateValidatorRangeTest {
+
+    private DateValidator dateValidator;
+
+    @Before
+    public void init() {
+        dateValidator = new DateValidator();
+    }
+
+    @Test
+    public void testDateWithinRange_1() {
+        assertTrue(dateValidator.isThisDateWithin1MonthRange("30/11/2019", "dd/MM/yyyy"));
+    }
+
+    @Test
+    public void testDateWithinRange_2() {
+        assertFalse(dateValidator.isThisDateWithin1MonthRange("31/9/2019", "dd/MM/yyyy"));
+    }
+
+    @Test
+    public void testDateWithinRange_3() {
+        assertTrue(dateValidator.isThisDateWithin1MonthRange("20/11/2019", "dd/MM/yyyy"));
+    }
+
+    @Test
+    public void testDateWithinRange_4() {
+        assertFalse(dateValidator.isThisDateWithin1MonthRange("21/1/2019", "dd/MM/yyyy"));
+    }
+}
